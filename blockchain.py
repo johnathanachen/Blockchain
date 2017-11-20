eccb4d3bce75f0b66a95501ba00a1a6d0fd05697
Johnathan Chen <johnathanchenn@outlook.com>
2017-11-20 06:49:53 -0800

    proof of work

--- blockchain.py
+++ blockchain.py
@@ -12,6 +12,20 @@ class Blockchain(object):
         # Create the genesis block
         self.new_block(previous_hash=1, proof=100)
 
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """ Validates the proof """
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256.(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
     def new_block(self, proof, previous_hash=None):
         # Creates a new Block and adds it to the chain
         block = {
