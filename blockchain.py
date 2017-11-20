25dee90ec74e5e0df6b079e3e86d5a721468642e
Johnathan Chen <johnathanchenn@outlook.com>
2017-11-19 19:38:59 -0800

    init

---
+++ blockchain.py
@@ -0,0 +1,8 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transaction = []
+
+    def new_block(self):
+        # Creates a new Block and adds it to the chain
+        pass
