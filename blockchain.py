ab8c8fd7f40336d79bf1454465fe139a8d0430fd
Johnathan Chen <johnathanchenn@outlook.com>
2017-11-20 07:10:34 -0800

    new transaction route

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,10 @@
 import hashlib
 import json
 from textwrap import dedent
-
 from time import time
 from uuid import uuid4
 
-from flask import Flask
+from flask import Flask, jsonify, request
 
 
 
@@ -82,7 +81,19 @@ def mine():
 
 @app.route('transactions/new', methods=['POST'])
 def new_transaction():
-    return "we'll add a new transaction"
+    values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required'):
+        return 'Missing values', 400
+
+    # Create a new Transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+
+    return jsonify(response), 201
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
